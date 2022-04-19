a1f8cac61cfc68af76949e909b569d4f2b8a5325
jinx <550961784@qq.com>
2022-04-19 10:02:48 +0800

    feat: 在_app.js中添加公用Head

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // 共享组件
+  // https://juejin.cn/post/7000351648459522078#heading-8
+  return (
+    <>
+      <Head>
+        <title>{Component.title || 'AI Lab'}</title>
+        <meta name="description" content={Component.description || ''} />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
