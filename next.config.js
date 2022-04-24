1a950e6cb322bd4f5d7c0053eff0e7061beeb01c
jinx <550961784@qq.com>
2022-04-24 11:16:37 +0800

    feat: 设置NextProvider 提供国际化

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   swcMinify: true,
   reactStrictMode: true,
+  i18n: {
+    locales: ['zh', 'en', 'zh-TW', 'jp', 'fr'],
+    defaultLocale: 'zh',
+    localeDetection: false // 是否开启自动检测语言
+  }
 }
 
 module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,41 @@
 import Head from 'next/head'
+import { NextIntlProvider, IntlErrorCode } from 'next-intl'
 import '../styles/globals.css'
+
+function onError(error) {
+  if (error.code === IntlErrorCode.MISSING_MESSAGE) {
+    // Missing translations are expected and should only log an error
+    console.error(error)
+  } else {
+    // Other errors indicate a bug in the app and should be reported
+    console.log('国际化其余错误')
+  }
+}
+
+function getMessageFallback({ namespace, key, error }) {
+  const path = [namespace, key].filter((part) => part != null).join('.')
+
+  if (error.code === IntlErrorCode.MISSING_MESSAGE) {
+    return `${path} 没有被翻译`
+  }
+  return `Dear developer, please fix this message: ${path}`
+}
 
 function MyApp({ Component, pageProps }) {
   // 共享组件
   // https://juejin.cn/post/7000351648459522078#heading-8
   return (
-    <>
+    <NextIntlProvider
+      messages={pageProps.messages}
+      timeZone="Asia/Shanghai"
+      onError={onError}
+      getMessageFallback={getMessageFallback}>
       <Head>
         <title>{Component.title || 'AI Lab'}</title>
         <meta name="description" content={Component.description || ''} />
       </Head>
       <Component {...pageProps} />
-    </>
+    </NextIntlProvider>
   )
 }
 
