05aae830809f20c4e24dc31f4e1d3de91d2cfff5
jinx <550961784@qq.com>
2022-04-24 11:23:42 +0800

    feat: Home页面国际化

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,32 @@
+import { useTranslations } from 'next-intl'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
 export default function Home() {
-  return <h1 className="text-3xl text-red-500 font-bold underline">Hello world!</h1>
+  const t = useTranslations('Index')
+  const { locale, locales, route } = useRouter()
+  console.log('locales', locales)
+  const otherLocale = locales?.find((cur) => cur !== locale)
+
+  return (
+    <div>
+      <h1 className="text-3xl text-red-500 font-bold underline">{t('title')}</h1>
+      <h3>locale: {locale}</h3>
+      {otherLocale && (
+        <Link href={route} locale={otherLocale}>
+          <a>切换</a>
+        </Link>
+      )}
+    </div>
+  )
+}
+
+export function getStaticProps({ locale }) {
+  return {
+    props: {
+      messages: {
+        ...require(`../messages/index/${locale}.json`),
+      },
+    },
+  }
 }
