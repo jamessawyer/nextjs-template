69b85c00d4edcfd00ad641593805476e591eb1bb
jinx <550961784@qq.com>
2022-04-25 11:23:46 +0800

    update: 将zh-TW 修改为 zh-Hant

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   swcMinify: true,
   reactStrictMode: true,
   i18n: {
-    locales: ['zh', 'en', 'zh-TW', 'jp', 'fr'],
+    locales: ['zh', 'en', 'zh-Hant', 'jp', 'fr'],
     defaultLocale: 'zh',
     localeDetection: false // 是否开启自动检测语言
   }
