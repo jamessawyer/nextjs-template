476d444c165031e76e29c4312514957d32e0071f
jinx <550961784@qq.com>
2022-04-26 14:51:20 +0800

    feat: 添加 ApplyIntro 申请介绍组件

---
+++ components/ApplyIntro.jsx
@@ -0,0 +1,33 @@
+import { useTranslations } from 'next-intl'
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+import { useCallback } from 'react'
+import ApplyCard from '../public/images/home/apply-card.png'
+
+export default function ApplyIntro() {
+  const t = useTranslations('Apply')
+  const { locale: currentLocal } = useRouter()
+
+  // 打开Modal框
+  const openModal = useCallback(() => {}, [])
+
+  return (
+    <section
+      id="apply"
+      className="w-full h-189 bg-[url('/images/home/apply-bg.png')] bg-[#132A4F] mix-blend-overlay bg-cover bg-center flex flex-row  gap-x-38 pl-48 pr-83 pt-40.5 pb-38">
+      <div className="shrink-0 w-88.5">
+        <Image src={ApplyCard} alt="" />
+      </div>
+      <div className="flex flex-col">
+        <h1 className="font-semibold text-4xl text-[#E5E5E5] tracking-wider">{t('title')}</h1>
+        <p className="font-medium text-[#E5E5E5] text-2.5xl mt-14">{t('content')}</p>
+        <button
+          type="button"
+          onClick={openModal}
+          className="w-46 h-14.5 leading-14.5 tracking-[0.26em] bg-[#972F17] text-center text-[#E5E5E5] text-1.5xl mt-auto">
+          {t('button')}
+        </button>
+      </div>
+    </section>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { useTranslations } from 'next-intl'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import ImageCarousel from '../components/ImageCarousel'
+import ApplyIntro from '../components/ApplyIntro'
 
 export default function Home() {
   const t = useTranslations('Index')
@@ -10,8 +11,9 @@ export default function Home() {
   const otherLocale = locales?.find((cur) => cur !== locale)
 
   return (
-    <div className="h-full">
+    <>
       <ImageCarousel />
+      <ApplyIntro />
       <h1 className="text-3xl text-red-500 font-bold underline">{t('title')}</h1>
       <h3>locale: {locale}</h3>
       <div className="h-[100vh] w-full bg-orange-400" />
@@ -21,7 +23,7 @@ export default function Home() {
           <a>切换</a>
         </Link>
       )}
-    </div>
+    </>
   )
 }
 
