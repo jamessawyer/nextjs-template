c74fde424bc8fd0c76be5bd0214704c9815ff602
jinx <550961784@qq.com>
2022-04-26 09:34:16 +0800

    perf: PageLayout 去掉多余的div标签

--- components/PageLayout.jsx
+++ components/PageLayout.jsx
@@ -3,10 +3,10 @@ import Navigation from './Navigation'
 
 export default function PageLayout({ children }) {
   return (
-    <div>
+    <>
       <Navigation />
       <main>{children}</main>
       <Footer />
-    </div>
+    </>
   )
 }
