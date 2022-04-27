4b0594b58e13f98e9eaa453cad82a9202b7d0d20
jinx <550961784@qq.com>
2022-04-27 20:10:19 +0800

    style: Navigation 2xl适配

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -11,46 +11,46 @@ export default function Navigation() {
 
   return (
     <header className="sticky top-0 left-0 right-0 z-10">
-      <section className="w-full h-15 bg-[#112541]" />
-      <section className="flex flex-row justify-between items-center bg-[#1A487F] pl-44 pr-28">
-        <div className="w-94 relative">
+      <section className="w-full h-15 bg-[#112541] 2xl:h-10" />
+      <section className="flex flex-row justify-between items-center bg-[#1A487F] pl-44 pr-28 2xl:pl-28">
+        <div className="w-94 relative 2xl:w-60">
           <Image src={LOGOS[currentLocal]} alt="HUST AI" priority />
         </div>
 
         <nav>
           <ul className="flex flex-row justify-between items-center cursor-pointer">
-            <li className="pr-9 py-16 nav-text">
+            <li className="pr-9 py-16 nav-text 2xl:pr-8 2xl:py-12">
               <a className="opacity-80 hover:opacity-100" href="#institution">
                 {t('institution')}
               </a>
             </li>
             <div className="w-px h-5 bg-[#D9E0E6] opacity-30" />
-            <li className="px-9 py-16 nav-text">
+            <li className="px-9 py-16 nav-text 2xl:px-8 2xl:py-12">
               <Link href={{ pathname: '/news', query }} as={asPath} locale={currentLocal}>
                 <a className="opacity-80 hover:opacity-100">{t('news')}</a>
               </Link>
             </li>
             <div className="w-px h-5 bg-[#D9E0E6] opacity-30" />
-            <li className="px-9 py-16 nav-text">
+            <li className="px-9 py-16 nav-text 2xl:px-8 2xl:py-12">
               <a className="opacity-80 hover:opacity-100" href="#scholar">
                 {t('scholar')}
               </a>
             </li>
             <div className="w-px h-5 bg-[#D9E0E6] opacity-30" />
-            <li className="px-9 py-16 nav-text">
+            <li className="px-9 py-16 nav-text 2xl:px-8 2xl:py-12">
               <a className="opacity-80 hover:opacity-100" href="#domain">
                 {t('domain')}
               </a>
             </li>
             <div className="w-px h-5 bg-[#D9E0E6] opacity-30" />
-            <li className="px-9 py-16 nav-text">
+            <li className="px-9 py-16 nav-text 2xl:px-8 2xl:py-12">
               <span className="opacity-80 hover:opacity-100">{t('apply')}</span>
             </li>
             <div className="w-px h-5 bg-[#D9E0E6] opacity-30" />
-            <li className="group relative pl-9 py-16 nav-text">
+            <li className="group relative pl-9 py-16 nav-text 2xl:pl-8 2xl:py-12">
               <div className="flex flex-row justify-between items-center gap-x-7 opacity-80 hover:opacity-100">
                 <span>{t('lang')}</span>
-                <div className="space-y-1.5">
+                <div className="space-y-1.5 2xl:space-y-1">
                   <div className="w-6 h-0.5 bg-[#E5E5E5]" />
                   <div className="w-6 h-0.5 bg-[#E5E5E5]" />
                   <div className="w-6 h-0.5 bg-[#E5E5E5]" />
@@ -60,13 +60,17 @@ export default function Navigation() {
                 {locales.map((locale) => {
                   if (locale === currentLocal) {
                     return (
-                      <li key={locale} className="w-48 h-18 shadow-inner pl-11 pt-3.5">
+                      <li
+                        key={locale}
+                        className="w-48 h-18 shadow-inner pl-11 pt-3.5 2xl:h-14 2xl:pt-4">
                         <span className="text-white opacity-100">{t('lang')}</span>
                       </li>
                     )
                   }
                   return (
-                    <li key={locale} className="w-48 h-18 shadow-inner pl-11 pt-3.5">
+                    <li
+                      key={locale}
+                      className="w-48 h-18 shadow-inner pl-11 pt-3.5 2xl:h-14 2xl:pt-4">
                       <Link href={{ pathname, query }} as={asPath} locale={locale}>
                         <a className="nav-text opacity-80 hover:opacity-100">{LANGS[locale]}</a>
                       </Link>
