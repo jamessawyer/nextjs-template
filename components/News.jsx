1490d0b96bc29457944ad96aba3b42f6ba25d4dc
jinx <550961784@qq.com>
2022-04-28 11:11:14 +0800

    style: 给 News 添加动画效果

--- components/News.jsx
+++ components/News.jsx
@@ -2,6 +2,7 @@ import { useTranslations } from 'next-intl'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { Fade } from 'react-awesome-reveal'
 import NewsCard from '../public/images/home/news-card.png'
 
 export default function News() {
@@ -17,27 +18,29 @@ export default function News() {
         2xl:h-158.5 2xl:gap-x-16 2xl:bg-[length:100%_39.625rem]
       ">
       <div className="flex flex-col">
-        <h1
-          className="
-          text-5.5xl text-white
-          2xl:text-4xl
-        ">
-          {t('title')}
-        </h1>
-        <h3
-          className="
-          text-4xl text-white mt-2 mb-1
-          2xl:text-2xl
-          ">
-          {t('subtitle1')}
-        </h3>
-        <h3
-          className="
-          text-4xl text-white
-          2xl:text-2xl
-        ">
-          {t('subtitle2')}
-        </h3>
+        <Fade cascade direction="up">
+          <h1
+            className="
+              text-5.5xl text-white
+              2xl:text-4xl
+            ">
+            {t('title')}
+          </h1>
+          <h3
+            className="
+              text-4xl text-white mt-2 mb-1
+              2xl:text-2xl
+              ">
+            {t('subtitle1')}
+          </h3>
+          <h3
+            className="
+              text-4xl text-white
+              2xl:text-2xl
+            ">
+            {t('subtitle2')}
+          </h3>
+        </Fade>
       </div>
       <div
         className="
@@ -78,16 +81,18 @@ export default function News() {
         </ul>
         <div className="relative grow">
           <Image src={NewsCard} layout="fill" alt="" />
-          <Link href={{ pathname: '/news' }} locale={currentLocal}>
-            <a
-              className="
-                absolute left-0 bottom-[75px] py-6 px-16 text-xs text-[#333333] tracking-[.2em]  bg-white flex flex-row items-center gap-x-4
-                hover:bg-[#000] hover:text-white transition-all
-            ">
-              <span>{t('button')}</span>
-              <span className="text-lg">→</span>
-            </a>
-          </Link>
+          <Fade direction="right" className="absolute left-0 bottom-[75px]">
+            <Link href={{ pathname: '/news' }} locale={currentLocal}>
+              <a
+                className="
+                  py-6 px-16 text-xs text-[#333333] tracking-[.2em]  bg-white flex flex-row items-center gap-x-4
+                  hover:bg-[#000] hover:text-white transition-all
+              ">
+                <span>{t('button')}</span>
+                <span className="text-lg">→</span>
+              </a>
+            </Link>
+          </Fade>
         </div>
       </div>
     </section>
