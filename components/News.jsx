c060077be52d6ffc81b71cc131d79b39b5010ea9
jinx <550961784@qq.com>
2022-04-28 10:58:04 +0800

    style: News适配2xl

--- components/News.jsx
+++ components/News.jsx
@@ -14,36 +14,64 @@ export default function News() {
         bg-[#E5E5E5] bg-[url('/images/home/news-bg.png')] bg-[length:100%_44.25rem] bg-left-top
         bg-no-repeat
         flex flex-row justify-center items-center gap-x-21.5
+        2xl:h-158.5 2xl:gap-x-16 2xl:bg-[length:100%_39.625rem]
       ">
       <div className="flex flex-col">
-        <h1 className="text-5.5xl text-white">{t('title')}</h1>
-        <h3 className="text-4xl text-white mt-2 mb-1">{t('subtitle1')}</h3>
-        <h3 className="text-4xl text-white">{t('subtitle2')}</h3>
+        <h1
+          className="
+          text-5.5xl text-white
+          2xl:text-4xl
+        ">
+          {t('title')}
+        </h1>
+        <h3
+          className="
+          text-4xl text-white mt-2 mb-1
+          2xl:text-2xl
+          ">
+          {t('subtitle1')}
+        </h3>
+        <h3
+          className="
+          text-4xl text-white
+          2xl:text-2xl
+        ">
+          {t('subtitle2')}
+        </h3>
       </div>
-      <div className="w-296.5 h-158.5 flex flex-row bg-white self-end overflow-hidden">
-        <ul className="w-119.25 h-full shadow-[0px_4px_25px_5px_rgba(0,0,0,0.1)] px-25.25 py-26.5 overflow-y-auto scrollbar-thin scrollbar-thumb-sky-200 scrollbar-track-gray-100">
+      <div
+        className="
+        w-296.5 h-158.5 flex flex-row bg-white self-end overflow-hidden
+        2xl:w-248 2xl:h-132.5
+        ">
+        <ul
+          className="
+          text-xl text-[#0E204E] w-119.25 h-full shadow-[0px_4px_25px_5px_rgba(0,0,0,0.1)] px-25.25 py-26.5 overflow-y-auto scrollbar-thin scrollbar-thumb-sky-200 scrollbar-track-gray-100
+          2xl:text-base 2xl:w-100 2xl:px-16 2xl:py-20
+          ">
           <li
-            className="text-xl text-[#0E204E] relative
-            before:content-[''] before:w-2.5 before:h-2.5 before:rounded-full before:bg-[#C4C4C4] before:absolute before:-left-6 before:top-2">
+            className="relative
+              before:content-[''] before:w-2.5 before:h-2.5 before:rounded-full before:bg-[#C4C4C4] before:absolute before:-left-6 before:top-2
+            ">
             {t('content1')}
           </li>
           <li
-            className="text-xl text-[#0E204E] relative
+            className="relative
             before:content-[''] before:w-2.5 before:h-2.5 before:rounded-full before:bg-[#C4C4C4] before:absolute before:-left-6 before:top-2">
             {t('content2')}
           </li>
           <li
-            className="text-xl text-[#0E204E] relative
+            className="relative
             before:content-[''] before:w-2.5 before:h-2.5 before:rounded-full before:bg-[#C4C4C4] before:absolute before:-left-6 before:top-2">
             {t('content3')}
           </li>
           <li
-            className="text-xl text-[#0E204E] relative
+            className="relative
             before:content-[''] before:w-2.5 before:h-2.5 before:rounded-full before:bg-[#C4C4C4] before:absolute before:-left-6 before:top-2">
             {t('content4')}
           </li>
           <li
-            className="text-xl text-[#0E204E] relative
+            className="relative
             before:content-[''] before:w-2.5 before:h-2.5 before:rounded-full before:bg-[#C4C4C4] before:absolute before:-left-6 before:top-2">
             {t('content5')}
           </li>
