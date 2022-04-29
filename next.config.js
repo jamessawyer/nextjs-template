9c4c7e79d4b21d3aa067bf20439e373a0b8110b0
jinx <550961784@qq.com>
2022-04-29 16:01:46 +0800

    update: 更新多语言顺序

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   swcMinify: true,
   reactStrictMode: true,
   i18n: {
-    locales: ['zh', 'en', 'zh-Hant', 'jp', 'fr'],
+    locales: ['zh', 'zh-Hant', 'en', 'fr', 'jp'],
     defaultLocale: 'zh',
     localeDetection: false // 是否开启自动检测语言
   }
