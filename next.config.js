985c23fe37b3105edb219126cdc8b17780901241
jinx <550961784@qq.com>
2022-05-06 11:26:07 +0800

    refactor: 暂时屏蔽其它语种,添加 experimental属性,以便支持docker打包

--- next.config.js
+++ next.config.js
@@ -3,9 +3,14 @@ const nextConfig = {
   swcMinify: true,
   reactStrictMode: true,
   i18n: {
-    locales: ['zh', 'zh-Hant', 'en', 'fr', 'jp'],
+    // locales: ['zh', 'zh-Hant', 'en', 'fr', 'jp'],
+    // locales: ['zh', 'en', 'jp'],
+    locales: ['zh'],
     defaultLocale: 'zh',
     localeDetection: false // 是否开启自动检测语言
+  },
+  experimental: {
+    outputStandalone: true,
   }
 }
 
