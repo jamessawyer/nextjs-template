32e6dfdcde3495cca4a5f148866bc1b1b6ed7d31
jinx <550961784@qq.com>
2022-05-27 13:55:37 +0800

    refactor(ImageCarousel): 更换轮播图图片&增加图片点击跳转

--- components/ImageCarousel.jsx
+++ components/ImageCarousel.jsx
@@ -1,10 +1,8 @@
 import { useTranslations } from 'next-intl'
-import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { memo } from 'react'
 import Slider from 'react-slick'
-import RightArrow from '../public/images/right-arrow.png'
 
 const settings = {
   dots: true,
@@ -27,31 +25,19 @@ function ImageCarousel() {
     <section>
       <section id="institution" className="relative h-139 w-full sm:hidden">
         <Slider {...settings}>
-          <div className="h-139 w-full bg-[url('/images/home/banner-1.png')] bg-cover bg-center" />
-          <div className="h-139 w-full bg-[url('/images/home/banner-2.png')] bg-cover bg-center" />
-          <div className="h-139 w-full bg-[url('/images/home/banner-3.png')] bg-cover bg-center" />
-        </Slider>
-        <div className="absolute left-0 bottom-16 bg-[#0c3655] bg-opacity-60 transition-all hover:bg-opacity-80">
-          <Link href={{ pathname: '/deep' }} locale={currentLocal}>
-            <a
-              className="
-              flex h-full w-full flex-row items-center gap-x-7 pl-39 pr-29 pt-15 pb-13
-              2xl:pl-24 2xl:pr-15 2xl:pt-8 2xl:pb-7
-            ">
-              <p
-                className="
-                max-w-[305px] text-2xl font-semibold text-[#E5E5E5]
-                hover:text-white
-                2xl:max-w-[190px] 2xl:text-xl
-                ">
-                {t('gallery-button')}
-              </p>
-              <div className="relative h-10 w-10 2xl:h-8 2xl:w-8">
-                <Image src={RightArrow} alt="" />
-              </div>
-            </a>
+          <Link href={{ pathname: '/article/1' }} locale={currentLocal} passHref>
+            <div className="h-139 w-full bg-[url('/images/home/banner-1.png')] bg-cover bg-center cursor-pointer" />
           </Link>
-        </div>
+          <Link href={{ pathname: '/article/2' }} locale={currentLocal} passHref>
+            <div className="h-139 w-full bg-[url('/images/home/banner-2.png')] bg-cover bg-center cursor-pointer" />
+          </Link>
+          <Link href={{ pathname: '/article/3' }} locale={currentLocal} passHref>
+            <div className="h-139 w-full bg-[url('/images/home/banner-3.png')] bg-cover bg-center cursor-pointer" />
+          </Link>
+          <Link href={{ pathname: '/article/4' }} locale={currentLocal} passHref>
+            <div className="h-139 w-full bg-[url('/images/home/banner-4.png')] bg-cover bg-center cursor-pointer" />
+          </Link>
+        </Slider>
       </section>
 
       <section className="mobile hidden sm:block sm:bg-[#0e204e] sm:pb-16">
